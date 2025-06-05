66d0690da5de6f89aacbd591dd11239b6154f725
Abdallah Elrashedy <abodyrody55@gmail.com>
2025-06-05 17:36:12 +0400

    fixes

--- src/app/app.routes.server.ts
+++ src/app/app.routes.server.ts
@@ -1,6 +1,24 @@
 import { RenderMode, ServerRoute } from '@angular/ssr';
+import { APP_ROUTES } from './core/constants/app-routes.enum';
 
 export const serverRoutes: ServerRoute[] = [
+  {
+    path: '',
+    renderMode: RenderMode.Prerender,
+  },
+  {
+    path: APP_ROUTES.HOME.slice(1),
+    renderMode: RenderMode.Prerender,
+  },
+  {
+    path: APP_ROUTES.PRODUCTS.slice(1),
+    renderMode: RenderMode.Server,
+  },
+  {
+    path: APP_ROUTES.PRODUCT_DETAILS.slice(1) + '/:slug',
+    renderMode: RenderMode.Server,
+  },
+
   {
     path: '**',
     renderMode: RenderMode.Client,
